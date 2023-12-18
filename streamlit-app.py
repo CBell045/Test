902a15cff64b7d72ffb508b424b19ffa051ed8fa
CBell045 <bel19006@byui.edu>
2023-12-18 08:48:53 -0700

    streamlit test

---
+++ streamlit-app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from auth import auth
+
+st.title("Home")
+
+supabase = auth()
+if supabase is None:
+    st.stop()
+
+user = supabase.auth.get_user()
+user_name = user.user.user_metadata['name']
+
+st.write(f"Hey {user_name}, welcome to your streamlit app!")
+
+
+# Sign out
+if st.button("Sign out"):
+    supabase.auth.sign_out()
+    st.success("Signed out successfully")
+    st.rerun()
